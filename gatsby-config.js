f43b7e8b94f0987f30b0b96eed23601aabf9acf1
Abhinay Pandey <abhinaypandey02@gmail.com>
2022-03-03 23:30:58 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,13 @@
+/** @type {import('gatsby').GatsbyConfig} */
+module.exports = {
+  siteMetadata: {
+      title: `new`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }]
+};
